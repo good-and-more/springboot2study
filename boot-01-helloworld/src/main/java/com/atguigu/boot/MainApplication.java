a9e18a9ea3d27384ebddd846a49079af209fba06
hyd <zht@gdu-tech.com>
2021-01-18 19:50:35 +0800

    first commit

---
+++ boot-01-helloworld/src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,23 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * 主程序类
+ * @SpringBootApplication：这是一个SpringBoot应用
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        //1.返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+        //2.查看容器里面所有的组件
+//        String[] names = run.getBeanDefinitionNames();
+//        for (String name: names) {
+//            System.out.println(name);
+//        }
+    }
+}
