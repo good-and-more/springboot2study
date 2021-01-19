9b5747f6c2c4468769d00203365ab6236ca54dd1
hyd <zht@gdu-tech.com>
2021-01-19 20:01:31 +0800

    web starter

---
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/bug.jpg")
+    public String hello(){
+        return "aaa";
+    }
+}
