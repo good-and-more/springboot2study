25d518bd446256b11ecc39b39f132f0d18edfa66
hyd <zht@gdu-tech.com>
2021-02-03 17:29:26 +0800

    增加yaml配置文件

--- boot-05-web-admin/src/test/java/com/atguigu/admin/Boot05WebAdminApplicationTests.java
+++ boot-05-web-admin/src/test/java/com/atguigu/admin/Boot05WebAdminApplicationTests.java
@@ -1,13 +1,18 @@
 package com.atguigu.admin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class Boot05WebAdminApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        jdbcTemplate.queryForObject
     }
-
 }
