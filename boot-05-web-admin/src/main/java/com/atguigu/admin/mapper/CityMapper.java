98861abd6b6385a032fa12e300da95da3544a8c1
learn_and_think <ilovesp20@outlook.com>
2021-02-19 19:48:15 +0800

    导入mybatis练习

--- boot-05-web-admin/src/main/java/com/atguigu/admin/mapper/CityMapper.java
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/mapper/CityMapper.java
@@ -1,7 +1,9 @@
 package com.atguigu.admin.mapper;
 
 import com.atguigu.admin.bean.City;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -9,6 +11,8 @@ public interface CityMapper {
     @Select("select * from city where id=#{id}")
     public City getById(Long id);
 
+    @Insert("insert into city(`name`,`state`,`country`) values(#{name},#{state},#{country})")
+    @Options(useGeneratedKeys = true,keyProperty = "id")
     public void insert(City city);
 
 }
