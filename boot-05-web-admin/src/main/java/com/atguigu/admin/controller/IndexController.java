ab5604a8af8f71bb1e53fb42ae158adbd6dbbfc7
learn_and_think <ilovesp20@outlook.com>
2021-02-26 17:41:20 +0800

    'junit'

--- boot-05-web-admin/src/main/java/com/atguigu/admin/controller/IndexController.java
+++ boot-05-web-admin/src/main/java/com/atguigu/admin/controller/IndexController.java
@@ -75,7 +75,7 @@ public class IndexController {
             //把登录成功的用户保存起来
             session.setAttribute("loginUser",user);
             //登录成功，重定向到main.html，重定向防止表单重复提交
-            return "redirect:/main.html";
+            return "redirect:/main";
         }
         model.addAttribute("msg", "账号密码错误");
         return "login";
@@ -86,7 +86,7 @@ public class IndexController {
      *
      * @return
      */
-    @GetMapping("/main.html")
+    @GetMapping("/main")
     public String mainPage(HttpSession session, Model model){
         //必须判断是否登录，不然用户直接访问main页面即可进入后台
         //是否登录，以后通过拦截器判断
@@ -100,7 +100,7 @@ public class IndexController {
 //        }
         ValueOperations<String, String> opsForValue = redisTemplate.opsForValue();
 
-        String s = opsForValue.get("/main.html");
+        String s = opsForValue.get("/main");
         String s1 = opsForValue.get("/sql");
 
         model.addAttribute("mainCount", s);
