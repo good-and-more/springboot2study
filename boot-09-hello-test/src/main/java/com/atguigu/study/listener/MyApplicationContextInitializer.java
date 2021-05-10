742c9187a568735a4be6e35c25e7b681cafa2a5f
learn_and_think <ilovesp20@outlook.com>
2021-05-10 22:45:49 +0800

    '自动逸监听器'

---
+++ boot-09-hello-test/src/main/java/com/atguigu/study/listener/MyApplicationContextInitializer.java
@@ -0,0 +1,11 @@
+package com.atguigu.study.listener;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class MyApplicationContextInitializer implements ApplicationContextInitializer {
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+        System.out.println("MyApplicationContextInitializer.....initialize...");
+    }
+}
